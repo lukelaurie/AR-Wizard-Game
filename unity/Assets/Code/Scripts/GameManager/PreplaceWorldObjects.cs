3a25ecf4bac28e64e08976990cbd689603d4aedb
Luke A Laurie <100880853+lukelaurie@users.noreply.github.com>
2024-10-07 21:27:10 -0700

    p

--- unity/Assets/Code/Scripts/GameManager/PreplaceWorldObjects.cs
+++ unity/Assets/Code/Scripts/GameManager/PreplaceWorldObjects.cs
@@ -1,48 +1,48 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Niantic.Experimental.Lightship.AR.WorldPositioning;
-using Niantic.Experimental.Lightship.AR.XRSubsystems;
-using UnityEngine;
-using UnityEngine.XR.ARKit;
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using Niantic.Experimental.Lightship.AR.WorldPositioning;
+// using Niantic.Experimental.Lightship.AR.XRSubsystems;
+// using UnityEngine;
+// using UnityEngine.XR.ARKit;
 
-public class PreplaceWorldObjects : MonoBehaviour
-{
-    [SerializeField] private List<GameObject> _possibleObjectsToPlace = new();
-    [SerializeField] private List<LatLong> _latLongs = new();
-    [SerializeField] private ARWorldPositioningManager _positioningManager;
-    [SerializeField] private ARWorldPositioningObjectHelper _objectHelper;
+// public class PreplaceWorldObjects : MonoBehaviour
+// {
+//     [SerializeField] private List<GameObject> _possibleObjectsToPlace = new();
+//     [SerializeField] private List<LatLong> _latLongs = new();
+//     [SerializeField] private ARWorldPositioningManager _positioningManager;
+//     [SerializeField] private ARWorldPositioningObjectHelper _objectHelper;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        foreach (var gpsCoord in _latLongs)
-        {
-            GameObject newObject = Instantiate(_possibleObjectsToPlace[_latLongs.IndexOf(gpsCoord) % _possibleObjectsToPlace.Count]);
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         foreach (var gpsCoord in _latLongs)
+//         {
+//             GameObject newObject = Instantiate(_possibleObjectsToPlace[_latLongs.IndexOf(gpsCoord) % _possibleObjectsToPlace.Count]);
 
-            _objectHelper.AddOrUpdateObject(newObject, gpsCoord.latitude, gpsCoord.longitude, 0, Quaternion.identity);
+//             _objectHelper.AddOrUpdateObject(newObject, gpsCoord.latitude, gpsCoord.longitude, 0, Quaternion.identity);
 
-            Debug.Log($"Added {newObject.name} with latitude {gpsCoord.latitude} and longitude {gpsCoord.longitude}");
-        }
+//             Debug.Log($"Added {newObject.name} with latitude {gpsCoord.latitude} and longitude {gpsCoord.longitude}");
+//         }
 
-        _positioningManager.OnStatusChanged += OnStatusChanged;
-    }
+//         _positioningManager.OnStatusChanged += OnStatusChanged;
+//     }
 
-    private void OnStatusChanged(WorldPositioningStatus status)
-    {
-        Debug.Log("Status changed to " + status);
-    }
+//     private void OnStatusChanged(WorldPositioningStatus status)
+//     {
+//         Debug.Log("Status changed to " + status);
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//     // Update is called once per frame
+//     void Update()
+//     {
 
-[System.Serializable]
-public struct LatLong
-{
-    public double latitude;
-    public double longitude;
-}
+//     }
+// }
+
+// [System.Serializable]
+// public struct LatLong
+// {
+//     public double latitude;
+//     public double longitude;
+// }
