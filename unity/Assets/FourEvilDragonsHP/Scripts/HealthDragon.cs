01d6caf0946c7e33659d29dec92577a98b6a3a82
conkip <ckip.games@gmail.com>
2024-10-19 13:25:37 -0700

    added some boss assets and starter scripts

---
+++ unity/Assets/FourEvilDragonsHP/Scripts/HealthDragon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonBossAttack : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount
+        if(health <= 0)
+        {
+            //play death animation
+            Destroy(gameObject);
+        }
+    }
+}
