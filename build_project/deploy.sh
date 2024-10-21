e241329a1b57d38497ac3a8fe21cf7d4a33d7ca6
Luke A Laurie <100880853+lukelaurie@users.noreply.github.com>
2024-10-20 17:12:33 -0700

    finished starter script

--- build_project/deploy.sh
+++ build_project/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-git pull
-
 git checkout multiplayer-online
+git pull
 cd AR-Wizard-Game/unity/game_build/
 
+chmod +x game.x86_64
 
-
-echo "hello world"
+# kill old server and start the new one 
+pkill -f game.x86_64
+nohup ./game.x86_64 -batchmode -nographics &
