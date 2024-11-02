f27c620c1d058223c441402ccea9a20bbe77118f
Luke A Laurie <100880853+lukelaurie@users.noreply.github.com>
2024-11-02 12:51:32 -0700

    update script

--- go_server/build_and_run.sh
+++ go_server/build_and_run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Define the PostgreSQL data directory
-DATA_DIR="/Library/PostgreSQL/16/data"
+# DATA_DIR="/Library/PostgreSQL/16/data"
+# DATA_DIR="C:/Program Files/PostgreSQL/16/data"
+DATA_DIR="/var/lib/postgresql/16/main"
 
 # Check if PostgreSQL is running
-# echo "Starting PostgreSQL..."
+echo "Starting PostgreSQL..."
 pg_ctl -D "$DATA_DIR" start > /dev/null 2>&1
 
+echo "Starting Go Sever..."
 go build ./cmd/app/main.go
-./main
+./main.exe
