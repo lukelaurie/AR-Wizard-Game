3f9b25d93104dcd62b4885b441aa3fc61ada2d98
Luke A Laurie <100880853+lukelaurie@users.noreply.github.com>
2024-11-03 10:04:39 -0700

    pipeline script

--- build_project/deploy.sh
+++ build_project/deploy.sh
@@ -5,16 +5,29 @@ cd AR-Wizard-Game/
 git checkout "$BRANCH_NAME"
 git pull
 
-cd unity/game_build/
 
+# Start up the unity server
+cd unity/game_build/
 chmod +x game.x86_64
 
 # kill old server and start the new one 
-pkill -f game.x86_64
+sudo pkill -f game.x86_64
 nohup ./game.x86_64 -batchmode -nographics > /dev/null 2>&1 &
 
-# Disown the process to detach it from the shell
-disown
 
+# start up the go server
+DATA_DIR="/var/lib/postgresql/16/main"
+
+# Check if PostgreSQL is running
+echo "Starting PostgreSQL..."
+sudo systemctl start postgresql
+
+echo "Starting Go Sever..."
+sudo pkill -f main
+go build ./cmd/app/main.go
+nohup ./main > /dev/null 2>&1 &
+
+
+disown
 # Exit the script
 exit 0
