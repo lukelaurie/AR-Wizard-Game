3a9f01b30ccec265481b2414223b72fe6c72f38b
Luis Felix <luisfelix1@arizona.edu>
2024-11-29 15:25:57 -0700

    merge

--- unity/Assets/SpellList.cs
+++ unity/Assets/SpellList.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SpellList : MonoBehaviour
 {
     private int[] spells = { 1, 0, 0, 0 };
@@ -10,7 +10,6 @@ public class SpellList : MonoBehaviour
     [SerializeField] private Button healButton;
     [SerializeField] private Button rockButton;
     [SerializeField] private Button tempButton;
-    // Start is called before the first frame update
     void Start()
     {
         Button[] buttons = { lightButton, healButton, rockButton, tempButton };
