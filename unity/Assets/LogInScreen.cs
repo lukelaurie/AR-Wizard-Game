6d4487684225eb1fa4a237b84ddc43c8de535fa8
Luis Felix <luisfelix1@arizona.edu>
2024-11-29 15:00:15 -0700

    Scripts

--- unity/Assets/LogInScreen.cs
+++ unity/Assets/LogInScreen.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogInScreen : MonoBehaviour
 {
+    [SerializeField] private Button createButton;
+    [SerializeField] private Button loginButton;
+    [SerializeField] private Canvas homeCanvas;
+    [SerializeField] private Canvas loginCanvas;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        loginButton.onClick.AddListener(() =>
+        {
+            homeCanvas.gameObject.SetActive(true);
+            loginCanvas.gameObject.SetActive(false);
+        });
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        createButton.onClick.AddListener(() =>
+        {
+            homeCanvas.gameObject.SetActive(true);
+            loginCanvas.gameObject.SetActive(false);
+        });
     }
 }
---
+++ unity/Assets/SpellList.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellList : MonoBehaviour
+{
+    private int[] spells = { 1, 0, 0, 0 };
+    [SerializeField] private Button fireButton;
+    [SerializeField] private Button lightButton;
+    [SerializeField] private Button healButton;
+    [SerializeField] private Button rockButton;
+    [SerializeField] private Button tempButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button[] buttons = { lightButton, healButton, rockButton, tempButton };
+        for (int i = 0; i < 4; i++)
+        {
+            if (spells[i] == 1)
+            {
+                buttons[i].interactable = true;
+                buttons[i].GetComponent<Image>().enabled = false;
+            }
+            else
+            {
+                buttons[i].interactable = false;
+                buttons[i].GetComponent<Image>().enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
