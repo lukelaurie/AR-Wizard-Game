0e02c6166183d33bc934f599584fd5bc27798a1b
conkip <ckip.games@gmail.com>
2025-06-22 02:36:35 -0700

    final pushed

--- unity/Assets/Code/Scripts/Util/GlobalConfig.cs
+++ unity/Assets/Code/Scripts/Util/GlobalConfig.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class GlobalConfig : MonoBehaviour
 {
-    public static string baseURL = "http://localhost:3000";
-    //public static string baseURL = "http://18.117.251.172:3000";
+    //public static string baseURL = "http://localhost:3000";
+    public static string baseURL = "http://18.223.116.55:3030";
 }
